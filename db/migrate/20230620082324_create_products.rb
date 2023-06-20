fcf82aed2424cfad83799d833daced0e251ea91d
developer <shpandey@bestpeers.com>
2023-06-20 13:54:23 +0530

    Added model

---
+++ db/migrate/20230620082324_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :brand_name
+      t.string :description
+      t.integer :price
+      t.integer :quantity
+      t.boolean :status
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
