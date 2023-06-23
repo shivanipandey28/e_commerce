49c00c9a658e56eeb004293a4fa792dc08b8596f
developer <shpandey@bestpeers.com>
2023-06-23 15:20:54 +0530

    Added association between user and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   #validates :name,:brand_name,:rating,:price,:description, presence: true
   belongs_to :category
+  belongs_to :user
   validates :category_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :products
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,6 +7,5 @@ class User < ApplicationRecord
   def set_defaults
     self.role ||= :user
   end
-
-   enum role: { admin: "admin", seller: "seller", buyer: "buyer"}   
+  enum role: { admin: "admin", seller: "seller", buyer: "buyer"}   
 end
